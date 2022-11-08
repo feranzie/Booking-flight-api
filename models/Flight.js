20650b594c4371d311252b3d73f92b94fba5e0e8
unknown <oladaviesferanmi@gmail.com>
2022-11-08 02:13:14 +0100

    tweaks

--- models/Flight.js
+++ models/Flight.js
@@ -1,7 +1,10 @@
-exports.Flights = [/*{
+const {v4: uuid}=require("uuid");
+
+exports.Flights = [{
+  id:uuid(),
   title: 'flight to Canada',
   time: '1PM',
   price: 26000,
   date: '26-06-2022',
-},*/
+},
 ];
